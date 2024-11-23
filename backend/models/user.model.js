39c6f64af4a6093d98733abfd1dd99e10b8c5262
Raviraj Utture <ravirajutture123@gmail.com>
2024-11-23 08:23:16 +0530

    user model completed.

---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userschema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    minLength:4
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  gender: {
+    type: String,
+    require: true,
+    enum: ["male", "female"]
+  },
+  profilePic: {
+    type: String,
+    default: "",
+  },
+});
+
+const User = mongoose.model("User",userschema);
+
+export default User;
